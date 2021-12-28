ea648cad8b11393389cd59e455312b133fcaa0c8
alpaca <zxcvbnm5288@gmail.com>
2021-12-28 13:30:48 +0900

    add recoil, styled-components and etc

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./assets/styles/themes";
+import GlobalStyle from "./assets/styles/global";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
