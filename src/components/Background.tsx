4d8c0935589a5a6486b98861640f69cc6e35c4db
alpaca <zxcvbnm5288@gmail.com>
2022-01-04 19:27:44 +0900

    improved speed issues when changing the theme

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,18 +1,10 @@
+import { useLayoutEffect, useState } from "react";
+import { useRecoilValue } from "recoil";
 import styled, { keyframes } from "styled-components";
-
-const multipleBoxShadow = (n: number, color: string) => {
-  let value = `${Math.floor(Math.random() * 2000 + 1)}px ${Math.floor(
-    Math.random() * 2000 + 1
-  )}px ${color}`;
-
-  for (let i = 2; i <= n; i++) {
-    value += `, ${Math.floor(Math.random() * 2000 + 1)}px ${Math.floor(
-      Math.random() * 2000 + 1
-    )}px ${color}`;
-  }
-
-  return value;
-};
+import { isDarkState } from "../atoms";
+interface BoxShadow {
+  boxShadow: any;
+}
 
 const animation = keyframes`
   from {
@@ -24,11 +16,11 @@ const animation = keyframes`
 	}
 `;
 
-const Star1 = styled.div`
+const Star1 = styled.div<BoxShadow>`
   width: 1px;
   height: 1px;
   background: transparent;
-  box-shadow: ${(props) => multipleBoxShadow(700, props.theme.starColor)};
+  box-shadow: ${(props) => props.boxShadow};
   animation: ${animation} 50s linear infinite;
 
   &:after {
@@ -38,15 +30,15 @@ const Star1 = styled.div`
     width: 1px;
     height: 1px;
     background: transparent;
-    box-shadow: ${(props) => multipleBoxShadow(700, props.theme.starColor)};
+    box-shadow: ${(props) => props.boxShadow};
   }
 `;
 
-const Star2 = styled.div`
+const Star2 = styled.div<BoxShadow>`
   width: 2px;
   height: 2px;
   background: transparent;
-  box-shadow: ${(props) => multipleBoxShadow(200, props.theme.starColor)};
+  box-shadow: ${(props) => props.boxShadow};
   animation: ${animation} 100s linear infinite;
 
   &:after {
@@ -56,15 +48,15 @@ const Star2 = styled.div`
     width: 2px;
     height: 2px;
     background: transparent;
-    box-shadow: ${(props) => multipleBoxShadow(200, props.theme.starColor)};
+    box-shadow: ${(props) => props.boxShadow};
   }
 `;
 
-const Star3 = styled.div`
+const Star3 = styled.div<BoxShadow>`
   width: 3px;
   height: 3px;
   background: transparent;
-  box-shadow: ${(props) => multipleBoxShadow(100, props.theme.starColor)};
+  box-shadow: ${(props) => props.boxShadow};
   animation: ${animation} 150s linear infinite;
 
   &:after {
@@ -74,7 +66,7 @@ const Star3 = styled.div`
     width: 3px;
     height: 3px;
     background: transparent;
-    box-shadow: ${(props) => multipleBoxShadow(100, props.theme.starColor)};
+    box-shadow: ${(props) => props.boxShadow};
   }
 `;
 
@@ -83,11 +75,42 @@ const BackgroundContainer = styled.div`
 `;
 
 function Background() {
+  const isDark = useRecoilValue(isDarkState);
+  const [darkThemeStars, setDarkThemeStars] = useState<String[]>([]);
+  const [lightThemeStars, setLightThemeStars] = useState<String[]>([]);
+
+  const multipleBoxShadow = (n: number, color: string) => {
+    let value = `${Math.floor(Math.random() * 2000 + 1)}px ${Math.floor(
+      Math.random() * 2000 + 1
+    )}px ${color}`;
+
+    for (let i = 2; i <= n; i++) {
+      value += `, ${Math.floor(Math.random() * 2000 + 1)}px ${Math.floor(
+        Math.random() * 2000 + 1
+      )}px ${color}`;
+    }
+
+    return value;
+  };
+
+  useLayoutEffect(() => {
+    setDarkThemeStars([
+      multipleBoxShadow(700, "#fff"),
+      multipleBoxShadow(200, "#fff"),
+      multipleBoxShadow(100, "#fff"),
+    ]);
+    setLightThemeStars([
+      multipleBoxShadow(700, "#000"),
+      multipleBoxShadow(200, "#000"),
+      multipleBoxShadow(100, "#000"),
+    ]);
+  }, []);
+
   return (
     <BackgroundContainer>
-      <Star1 />
-      <Star2 />
-      <Star3 />
+      <Star1 boxShadow={isDark ? darkThemeStars[0] : lightThemeStars[0]} />
+      <Star2 boxShadow={isDark ? darkThemeStars[1] : lightThemeStars[1]} />
+      <Star3 boxShadow={isDark ? darkThemeStars[2] : lightThemeStars[2]} />
     </BackgroundContainer>
   );
 }
