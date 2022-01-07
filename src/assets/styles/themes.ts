bf2c3e4f8ff47210b2dc2776f73a120fb7e4fa6d
alpaca <zxcvbnm5288@gmail.com>
2022-01-07 14:36:11 +0900

    update theme color

--- src/assets/styles/themes.ts
+++ src/assets/styles/themes.ts
@@ -8,9 +8,8 @@ export const darkTheme: DefaultTheme = {
   switcherThumbColor: "#1a1b1b",
   headerBgColor: "rgba(0, 0, 0, 0.5)",
   hamburgerBgColor: "rgb(23, 23, 24)",
-  hamburgerHoverColor: 'rgb(47, 48, 49)',
-
-  textColor: "#fff",
+  hamburgerHoverColor: "rgb(47, 48, 49)",
+  textColor: "#ecf0f1",
 };
 
 export const lightTheme: DefaultTheme = {
@@ -21,7 +20,6 @@ export const lightTheme: DefaultTheme = {
   switcherThumbColor: "#fff",
   headerBgColor: "rgba(255, 255, 255, 0.5)",
   hamburgerBgColor: "rgb(255, 255, 255)",
-  hamburgerHoverColor: 'rgb(242, 242, 242)',
-
-  textColor: "#000",
+  hamburgerHoverColor: "rgb(242, 242, 242)",
+  textColor: "#1e272e",
 };
